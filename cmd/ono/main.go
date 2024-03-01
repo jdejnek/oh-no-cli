f68f14049b2827ec24633b82693a147b2e9a6d49
jdejnek <jkdejnekbnet@gmail.com>
2024-03-01 20:19:37 +0100

    initial commit

---
+++ cmd/ono/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/jdejnek/ono/internal/commands"
+	"github.com/urfave/cli/v2"
+)
+
+// commands: get, create, ls, delete
+// trigger verb from commands
+// format url from flags
+
+func main() {
+	app := &cli.App{
+		Name:     "oh-no-cli",
+		Usage:    "Query the oh-no-api",
+		Commands: commands.Commands,
+	}
+
+	PrintColor := "\033[1;33m%s\033[0m %s"
+	cli.AppHelpTemplate = fmt.Sprintf(PrintColor,
+		`
+   ____     __  __           _   __   ____           ______    __     ____
+  / __ \   / / / /          / | / /  / __ \         / ____/   / /    /  _/
+ / / / /  / /_/ /  ______  /  |/ /  / / / / ______ / /       / /     / /  
+/ /_/ /  / __  /  /_____/ / /|  /  / /_/ / /_____// /___    / /___ _/ /   
+\____/  /_/ /_/          /_/ |_/   \____/         \____/   /_____//___/   
+                                                                          
+`, cli.AppHelpTemplate)
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal(err)
+	}
+}
